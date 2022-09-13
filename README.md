3a054a172e36ebead64e42f64eba24df9f346807
afolabiopakunle <opakunleafolabi@gmail.com>
2022-09-13 23:42:22 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-4f1iqd
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-4f1iqd)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import AddUser from "./components/Users/AddUser";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+     <AddUser />
+    </div>
+  );
+}
